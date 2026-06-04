2965a9963ea79af09a53374d5831a85beb6741cf
Mugi <37186888+ciss84@users.noreply.github.com>
2026-06-04 10:22:33 +0200

    Update main.cpp

--- Plugin_samples/Plugin-Loader-v1.13/source/main.cpp
+++ Plugin_samples/Plugin-Loader-v1.13/source/main.cpp
@@ -443,7 +443,7 @@ static void inject_into_game(pid_t pid, const char *title_id,
 
 int main()
 {
-    plugin_log("=== PLUGIN LOADER v1.13.3 + BACKPORK ===");
+    plugin_log("=== PLUGIN LOADER v1.13.4 + BACKPORK ===");
 
     payload_args_t *args = payload_get_args();
     kernel_base = args->kdata_base_addr;
@@ -477,7 +477,7 @@ int main()
         return -1;
     }
 
-    printf_notification("Plugin Loader v1.13.3: started     \nBy @84Ciss ");
+    printf_notification("Plugin Loader v1.13.4: started     \nBy @84Ciss ");
     plugin_log("Monitoring SceSysCore.elf (pid %d)...", syscore_pid);
 
     pid_t child_pid = -1;
