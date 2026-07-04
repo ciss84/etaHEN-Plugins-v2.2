359b08a3cce7671bc6597a93f2a2429378c9bcfe
Mugi <37186888+ciss84@users.noreply.github.com>
2026-07-04 15:28:16 +0200

    fix

--- include/hijacker/patch_shellcore.hpp
+++ include/hijacker/patch_shellcore.hpp
@@ -131,13 +131,8 @@ static pid_t sc_find_shellcore_pid()
     return pid;
 }
 
-static bool patch_shellcore_for_data(bool allow_ftp_dev_access = true)
+static bool patch_shellcore_for_data()
 {
-    if (!allow_ftp_dev_access) {
-        plugin_log("[SC_PATCH] ALLOW_FTP_DEV_ACCESS disabled, skipping patch");
-        return false;
-    }
-
     static bool done = false;
     if (done) return true;
     uint32_t fw = kernel_get_fw_version();
